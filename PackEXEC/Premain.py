8ace0c91676d76c10516943faaf220c64f195cbd
Sniper099 <nj.nava.99@gmail.com>
2020-04-19 01:10:02 +0100

    Last change.0

--- PackEXEC/Premain.py
+++ PackEXEC/Premain.py
@@ -1,16 +1,28 @@
 from PackDATA import *
+import Proc.py
 def UserIntercat():
     AffZone()
     print('*********Veuillez choisie une Zone Touristique S Il VOUS PLAIT********')
     zon=string(input('Veuillez entrer le nom de La Zone Touristique'))   
     SelectZone(zon)
-    print('----------La Zone Touristique choisie est La Zone de :', zon, '\n')
-    print('Pour lister toutes les site de la Zone Touristique ',zon,'Cliquez sur: \t 1 \n')
-    print('Pour lister les sites de La Zone Touristique ',zon,'Selon Categorie Cliquez sur: \t 2 \n')
-    print('Pour lister les sites de La Zone Touristique ',zon,'Selon Theme Cliquez sur: \t 3 \n')
-    print('Pour lister les sites de La Zone Touristique ',zon,'qui proposent une Visite Guidée Cliquez sur: \t 4 \n')
-    Number=int(input('Entrer le nom de Service désiré'))
-    #Selon les Numbers on aura des actions
-    
+    Number=0
+    while(Number!=5):
+        print('----------La Zone Touristique choisie est La Zone de :', zon, '\n')
+        print('Pour lister toutes les site de la Zone Touristique ',zon,'Cliquez sur: \t 1 \n')
+        print('Pour lister les sites de La Zone Touristique ',zon,'Selon Categorie Cliquez sur: \t 2 \n')
+        print('Pour lister les sites de La Zone Touristique ',zon,'Selon Theme Cliquez sur: \t 3 \n')
+        print('Pour lister les sites de La Zone Touristique ',zon,'qui proposent une Visite Guidée Cliquez sur: \t 4 \n')
+        AffZoneNumber=int(input('Entrer le nom de Service désiré'))
+        #Selon les Numbers on aura des actions
+        if(Number==1):
+            AffSites(zon)
+        elif(Number==2):
+            Category(zon)
+        elif(Number==3):
+            Theme(zon)
+        elif(Number==4):
+            Visite(zon)
+        else:
+            break
 
     
--- PackEXEC/main.py
+++ PackEXEC/main.py
@@ -1,3 +1,10 @@
 from PackDATA import *
 from PackETEN import *
+import Premain.py
+def main():
+    print('****************************** Bienvenue sur notre gestionnaire des Zones Touristiques du MAROC...Heureuse de vous acceuillir et vous servir!****************************** \n')
+    print('### Les zones offertes pour le temps actuel : {Rabat-Salé,Tiznit}###\n')
+    UserInteract()
+    print('Merci pour votre passage Heureuse de vous acceuillir et prete pour la refaire à bientot')
+    
 
